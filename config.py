de0e20e62167f06d44bf77a4850d25430a4a05f7
ADI GUPTA <vlogzx08@gmail.com>
2024-05-22 11:06:27 +0530

    Update config.py

--- config.py
+++ config.py
@@ -1,10 +1,10 @@
 import os
 class Config:
-    API_ID=22981583
-    API_HASH="c02ca0834d5d92ffb31fea2b50060195"
-    TOKEN="7156025226:AAFaTvFJwfkHLGcP2JZqY6GbfnUfajkQY5o"
-    SUDO = list(int(i) for i in os.environ.get("SUDO", "6752444356").split(" "))
-    START_IMG="https://te.legra.ph/file/735920504b44b3565242a.jpg"
-    BOT_ID=7156025226
-    BOT_USERNAME="SOMEONE47BOT"
-    BOT_NAME="SOMEONE47BOT"
+    API_ID=21446101
+    API_HASH="0a2594b17b59f8928aa240825d898df1"
+    TOKEN="6991755577:AAFpcHnRtFA48hhUZWlBCsRJIwP0P2yVRM8"
+    SUDO = list(int(i) for i in os.environ.get("SUDO", "6942143857").split(" "))
+    START_IMG="https://telegra.ph/file/268202321956f6fc5238e.jpg"
+    BOT_ID=6991755577
+    BOT_USERNAME="COPY_RIGHT_PROTECTOR_ll_BOT"
+    BOT_NAME="COPYRIGHT PROTECTOR"
